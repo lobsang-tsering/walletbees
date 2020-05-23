f9040de7293da10665275e7d13bbd0d03376d0cc
lobsangtsering <iamlobsangtsering@gmail.com>
2020-05-23 20:59:12 +0530

    Updates

---
+++ precache-manifest.8f42a6eccb1b4b993cc41ddb8864bbd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561c4f756b01fac578e1955cdb4bbcd0",
+    "url": "/lobsangTsering/walletbees/index.html"
+  },
+  {
+    "revision": "fa6b109d78ff40ce3a50",
+    "url": "/lobsangTsering/walletbees/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "606cb7982b69b919f1d7",
+    "url": "/lobsangTsering/walletbees/static/js/2.83b17038.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lobsangTsering/walletbees/static/js/2.83b17038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa6b109d78ff40ce3a50",
+    "url": "/lobsangTsering/walletbees/static/js/main.f4d76b5d.chunk.js"
+  },
+  {
+    "revision": "300d6f352bba8f474e66",
+    "url": "/lobsangTsering/walletbees/static/js/runtime-main.fdc0952a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lobsangTsering/walletbees/static/media/logo.5d5d9eef.svg"
+  }
+]);
